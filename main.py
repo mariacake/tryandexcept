19ec38fb64f5d937a9956508b7b1afc9421ca552
maria <maria.zanescu@gmail.com>
2023-02-22 13:07:25 +0200

    def while max min

--- main.py
+++ main.py
@@ -4,24 +4,40 @@
 #if x > 10:
  #   print("mai mare decat 10")
 
-try:
-    print(x)
-except:
-    print("no value added")
+#try:
+ #   print(x)
+#except:
+ #   print("no value added")
 
 #try and except for faulty code
 
-big = max("Maria e Frumoasa")
-print(big)
+#big = max("Maria e Frumoasa")
+#print(big)
 
-small = min("fgwgfxukwilfmelxwfnxqwgifhmlwie")
-print(small)
+#small = min("fgwgfxukwilfmelxwfnxqwgifhmlwie")
+#print(small)
 
-a = min("Hh")
-print(a)
+#a = min("Hh")
+#print(a)
 
-def maria():
-    print('maria e frumoasa')
-    print('baajakjodjwidio')
-maria()
+#def maria():
+  #  print('maria e frumoasa')
+ #   print('baajakjodjwidio')
+#maria()
 
+
+
+def mar(a):
+    if a < 868463583745:
+        print("bla bla")
+    else:
+        print("mai mare ca 5")
+
+a = 38283523985
+mar(a)
+print(id(mar))
+
+n = 10
+while n>1:
+    print("inc n am ajuns")
+    n = n * 10 - 100
